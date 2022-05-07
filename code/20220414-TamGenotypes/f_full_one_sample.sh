373673fee949566c704c1562033163bae2cd4ad9
RA <numpde@null.net>
2022-05-07 11:43:55 +0200

    dump

--- code/20220414-TamGenotypes/f_full_one_sample.sh
+++ code/20220414-TamGenotypes/f_full_one_sample.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-sample="${1:-SRR8375401}" # ALS (ALS-Ox)	67.0	Female	Bulbar
-sample="${1:-SRR8375402}" # ALS (Discordant)	74.0	Male	Limb
-sample="${1:-SRR8375403}" # ALS (Discordant)	60.0	Male	Limb
-sample="${1:-SRR8375405}" # ALS (ALS-Ox)	65.0	Female	Limb
+set -e
+
 sample="${1:-SRR8375406}" # other_neuro (ALS-Glia)	72.0	Female	Unknown
 sample="${1:-SRR8375426}" # ALS (ALS-Glia)	63.0	Female	Limb
 sample="${1:-SRR8375409}" # ALS (ALS-Ox)	76.0	Female	Unknown
@@ -11,6 +9,10 @@ sample="${1:-SRR8375359}" # ALS (ALS-Ox)	69.0	Female	Bulbar
 sample="${1:-SRR8375274}" # ALS (ALS-Ox)	60.0	Female	Limb
 sample="${1:-SRR8375398}" # non-neuro
 sample="${1:-SRR8375295}" # non-neuro
+sample="${1:-SRR8375405}" # ALS (ALS-Ox)	65.0	Female	Limb
+sample="${1:-SRR8375403}" # ALS (Discordant)	60.0	Male	Limb
+sample="${1:-SRR8375401}" # ALS (ALS-Ox)	67.0	Female	Bulbar
+sample="${1:-SRR8375402}" # ALS (Discordant)	74.0	Male	Limb
 
 echo "${sample}" &&
   bash d_1_download_sample.sh "${sample}" &&
